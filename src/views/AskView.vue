465d2745f1caf95e05d0f4a02808e64fad0260ac
wangsungtaek <tjdxorv@naver.com>
2022-02-27 08:49:43 +0900

    라우터 적용 및 API 구현

---
+++ src/views/AskView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div v-for="ask in askList" v-bind:key="ask.title">{{ ask.title }}</div>
+    </div>
+</template>
+
+<script>
+import { fetchAskList } from '../api/index.js';
+
+export default {
+    data() {
+        return {
+            askList: []
+        }
+    },
+    created() {
+        fetchAskList()
+            .then(response => (this.askList = response.data))
+            .catch(error => console.log(error));
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/NewsView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div v-for="user in users" v-bind:key="user.title">{{ user.title }}</div>
+    </div>
+</template>
+
+<script>
+import { fetchNewsList } from '../api/index.js';
+
+export default {
+    data() {
+        return {
+            users: []
+        }
+    },
+    created() {
+        fetchNewsList()
+            .then(response => (this.users = response.data))
+            .catch(error => (console.log(error)));
+    }
+}
+</script>
+
+<style>
+
+</style>
