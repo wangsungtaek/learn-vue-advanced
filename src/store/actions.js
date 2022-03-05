d05daa37eb4bcde2e12a81b3da62bed38fdc167b
wangsungtaek <tjdxorv@naver.com>
2022-03-05 11:33:39 +0900

    비동기 로직 async awit으로 변경

--- src/store/actions.js
+++ src/store/actions.js
@@ -8,60 +8,58 @@ import {
 } from '../api/index.js';
 
 export default {
-    FETCH_NEWS({ commit }) {
-        return fetchNewsList()
-            .then(({ data }) => {
-                commit('SET_NEWS', data);
-                return data;
-            })
-            .catch(error => {
-                console.log(error)
-            });
+    async FETCH_NEWS({ commit }) {
+        try {
+            const response = await fetchNewsList();
+            commit('SET_NEWS', response.data);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
     },
-    FETCH_ASK({ commit }) {
-        return fetchAskList()
-            .then(({ data }) => {
-                commit('SET_ASK', data);
-            })
-            .catch(error => { 
-                console.log(error)
-            });
+    async FETCH_ASK({ commit }) {
+        try {
+            const response = await fetchAskList();
+            commit('SET_ASK', response.data);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
     },
-    FETCH_JOBS({ commit }) {
-        return fetchJobsList()
-            .then(({ data }) => {
-                commit('SET_JOBS', data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    async FETCH_JOBS({ commit }) {
+        try {
+            const response = await fetchJobsList();
+            commit('SET_JOBS', response.data);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
     },
-    FETCH_USER({ commit }, username) {
-        fetchUserInfo(username)
-            .then(({data}) => {
-                commit('SET_USER', data);
-            })
-            .catch(error => {
-                console.log(error);
-            })
+    async FETCH_USER({ commit }, username) {
+        try {
+            const response = await fetchUserInfo(username);
+            commit('SET_USER', response.data);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
     },
-    FETCH_ITEM({ commit }, askid) {
-        return fetchAskInfo(askid).
-            then(({data}) => {
-                commit('SET_ITEM', data);
-            })
-            .catch(error => {
-                console.log(error);
-            })
+    async FETCH_ITEM({ commit }, askid) {
+        try {
+            const response = await fetchAskInfo(askid);
+            commit('SET_ITEM', response.data);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
     },
-    FETCH_LIST({ commit }, pageName) {
-        return fetchList(pageName).
-            then(({data}) => {
-                commit('SET_LIST', data);
-            })
-            .catch(error => {
-                console.log(error);
-            })
+    async FETCH_LIST({ commit }, pageName) {
+        try {
+            const response = await fetchList(pageName);
+            commit('SET_LIST', response.data);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
     }
-
 }
